c05b909c30c3be4aa627e27f804cd2303d8e7d85
ajlow2000 <ajlow2000@gmail.com>
2024-05-23 22:17:53 -0600

    feat: Reorgnize handlers

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/Ajlow2000/dacrib/components"
+	"github.com/Ajlow2000/dacrib/handlers"
 
 	"github.com/a-h/templ"
 )
@@ -15,7 +15,10 @@ import (
 func main() {
     port := "3333"
 
-	http.Handle("/", templ.Handler(components.HomePage()))
+	http.Handle("/", templ.Handler(handlers.Home()))
+	// http.Handle("/articles", nil)
+	// http.Handle("/experience", nil)
+	// http.Handle("/about", nil)
 
     fmt.Printf("Initialized server\n")
     err := http.ListenAndServe(":" + port, nil)
