e42c35d453a36498952a024733feb3496abe170d
ajlow2000 <ajlow2000@gmail.com>
2024-05-26 11:30:16 -0600

    feat: Add hello world stdlib http

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    io.WriteString(w, "Hello mom!")
+}
+
 func main() {
-	fmt.Println("Hello flake")
+
+    port := "3333"
+
+
+    http.HandleFunc("/", helloHandler)
+    // http.Handle("/articles", nil)
+    // http.Handle("/experience", nil)
+    // http.Handle("/about", nil)
+
+    fmt.Printf("Initialized server\n")
+    err := http.ListenAndServe(":" + port, nil)
+    if errors.Is(err, http.ErrServerClosed) {
+        fmt.Printf("server closed\n")
+    } else if err != nil {
+        fmt.Printf("error starting server: %s\n", err)
+        os.Exit(1)
+    }
 }
